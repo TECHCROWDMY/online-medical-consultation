5d863df7945f0c4007c9896890e29187ce1de7a8
Thilak7c <61497511+Thilak7c@users.noreply.github.com>
2021-07-20 00:17:30 +0800

    Delete models directory

--- models/user.dart
+++
@@ -1,9 +0,0 @@
-class User {
-  int id;
-  String name;
-  String avatar;
-
-  User({this.id, this.name, this.avatar});
-  User.fromJson(Map<String, dynamic> json)
-      : this(id: json['id'], name: json['name'], avatar: json['avatar']);
-}
